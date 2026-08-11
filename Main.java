10f235849fa4e0e0aee0d996321774cb0abe8c1f
Eric McElrea <mcelrea@Erics-MacBook-Air.local>
2026-08-11 11:15:21 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        Book b1 = new Book("Eye of the World", "Robert Jordan", 1997);
+
+        System.out.println(b1);
+    }
+}
